445262447a43f5e7b286204b4b61e33d6906f9df
kyledobash <kyledobash@yahoo.com>
2021-04-19 15:22:29 -0700

    went over arrays

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,7 +7,7 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var numbers = new List<double>() {12.7, 10.3, 6.11, 4.1};
+            var numbers = new [] {12.7, 10.3, 6.11, 4.1};
 
             var result = 0.0;
             foreach(var number in numbers)
